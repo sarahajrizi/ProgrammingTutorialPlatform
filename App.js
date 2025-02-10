7d0886d29130019467bff6bfcc1189db5a4e2cbd
sarahajrizi <sarahajrizi4@gmail.com>
2025-02-10 18:51:54 +0100

    adding context

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './src/navigation/MainNavigator';
 import { ProgressProvider } from './src/components/ProgressContext';
 
+
 export default function App() {
   return (
     <ProgressProvider>
